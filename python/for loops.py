6c5ebe197784da2976421ac7da2b3f8ee74e0faa
Anton Fate <breaddy16@gmail.com>
2021-05-13 21:31:49 -0500

    for loops.py

--- python/for loops.py
+++ python/for loops.py
@@ -1,8 +1,9 @@
-listy_list = [1,2,3,4,5,6,7,8,9,10]
-special = [1,5]
-for x in listy_list:
-    print(x)
-    if (x == 4):
-        print("jhin best champ")
-    elif(x in special): # this I learned is how to cross check a list way to late
-        print("kappa")
+def kap():
+    listy_list = [1,2,3,4,5,6,7,8,9,10]
+    special = [1,5]
+    for x in listy_list:
+        print(x)
+        if (x == 4):
+            print("jhin best champ")
+        elif(x in special): # this I learned is how to cross check a list way to late
+            print("kappa")
