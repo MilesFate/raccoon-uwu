9717264b796b7935346b3684945d1ee8da9fc05e
Anton Fate <breaddy16@gmail.com>
2021-05-15 17:01:37 -0500

    Main_Interface.py

---
+++ python/SCP Local Database/SCP_data.py
@@ -0,0 +1,89 @@
+def scp_173():
+    print(
+    """
+    Item #: SCP-173
+    -----------------
+    Object Class: Euclid
+    ----------------------
+    Special Containment Procedures: 
+    Item SCP-173 is to be kept in a locked container at all times. 
+    When personnel must enter SCP-173's container, no fewer than 3 may enter 
+    at any time and the door is to be relocked behind them. 
+    At all times, two persons must maintain direct eye contact with SCP-173 
+    until all personnel have vacated and relocked the container.
+    ------------------------------------------------------------------------------
+    Description: 
+    Moved to Site-19 1993. Origin is as of yet unknown. 
+    It is constructed from concrete and rebar with traces of Krylon brand spray paint. 
+    SCP-173 is animate and extremely hostile. The object cannot move while within a direct line of sight. 
+    Line of sight must not be broken at any time with SCP-173. 
+    Personnel assigned to enter container are instructed to alert one another before blinking. 
+    Object is reported to attack by snapping the neck at the base of the skull, or by strangulation. 
+    In the event of an attack, personnel are to observe Class 4 hazardous object containment procedures.
+    -------------------------------------------------------------------------------------------------------------
+    Personnel report sounds of scraping stone originating from within the container when no one is present inside. 
+    This is considered normal, and any change in this behaviour should be reported to the acting HMCL 
+    supervisor on duty.
+    -----------------------------------------------------------------------------------------
+    The reddish brown substance on the floor is a combination of feces and blood. 
+    Origin of these materials is unknown. The enclosure must be cleaned on a bi-weekly basis.
+    """)
+
+def scp_096():
+    print("""
+    Item #: SCP-096
+    -----------------
+    Object Class: Euclid
+    ---------------------
+    Special Containment Procedures: SCP-096 is to be contained in its cell, 
+    a 5m x 5m x 5m airtight steel cube, at all times. 
+    Weekly checks for any cracks or holes are mandatory. 
+    There are to be absolutely no video surveillance or optical tools of any kind inside SCP-096's cell. 
+    Security personnel will use pre-installed pressure sensors and laser detectors to ensure 
+    SCP-096's presence inside the cell.
+    Any and all photos, video, or recordings of SCP-096's likeness are strictly forbidden 
+    without approval from Dr. ███ and O5-█.
+    ------------------------------------------------------------------------------------------------------
+    Description: SCP-096 is a humanoid creature measuring approximately 2.38 meters in height. 
+    Subject shows very little muscle mass, with preliminary analysis of body mass 
+    suggesting mild malnutrition. 
+    Arms are grossly out of proportion with the rest of the subject's body, with an 
+    approximate length of 1.5 meters each. 
+    Skin is mostly devoid of pigmentation, with no sign of any body hair.
+    ------------------------------------------------------------------------------------------------------
+    SCP-096's jaw can open to four (4) times the norm of an average human. 
+    Other facial features remain similar to an average human, with the exception of the eyes, 
+    which are also devoid of pigmentation. 
+    It is not yet known whether SCP-096 is blind or not. It shows no signs of any higher brain functions, 
+    and is not considered to be sapient.
+    ------------------------------------------------------------------------------------------------------
+    SCP-096 is normally extremely docile, with pressure sensors inside its cell 
+    indicating it spends most of the day pacing by the eastern wall. 
+    However, when someone views SCP-096's face, whether it be directly, via video recording, 
+    or even a photograph, it will enter a stage of considerable emotional distress. 
+    SCP-096 will cover its face with its hands and begin screaming, crying, and babbling incoherently. 
+    Approximately one (1) to two (2) minutes after the first viewing, 
+    SCP-096 will begin running to the person who viewed its face (who will from this point on be 
+    referred to as SCP-096-1).
+    ------------------------------------------------------------------------------------------------------
+    Documented speeds have varied from thirty-five (35) km/h to ███ km/h, and seems to depend on 
+    distance from SCP-096-1. 
+    At this point, no known material or method can impede SCP-096's progress. 
+    The actual position of SCP-096-1 does not seem to affect SCP-096's response; it seems to have an 
+    innate sense of SCP-096-1's location. 
+    Note: This reaction does not occur when viewing artistic depictions .
+    ------------------------------------------------------------------------------------------------------
+    Upon arriving at SCP-096-1's location, SCP-096 will proceed to kill and [DATA EXPUNGED] SCP-096-1. 100% 
+    of cases have left no traces of SCP-096-1. 
+    SCP-096 will then sit down for several minutes before regaining its composure and becoming docile once again. 
+    It will then attempt to make its way back to its natural habitat, [DATA REDACTED]
+    ------------------------------------------------------------------------------------------------------
+    Due to the possibility of a mass chain reaction, including breach of Foundation secrecy and large 
+    civilian loss of life, 
+    retrieval of subject should be considered Alpha priority.
+    ------------------------------------------------------------------------------------------------------
+    Dr. ███ has also petitioned for immediate termination of SCP-096. 
+    Order is awaiting approval. Termination order has been approved, and is to be carried out by 
+    Dr. ███ on [DATA REDACTED]. 
+    """)
+
